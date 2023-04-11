3790f16fe9178b4fffb59d66d88f00fcc80914fc
CoToYo <56789038+CoToYo@users.noreply.github.com>
2023-04-11 19:06:19 -0400

    cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "localhost:8080 || https://tubular-kleicha-14687e.netlify.app"
-    origin '*'
+    origins '*'
 
     resource "*",
       headers: :any,
